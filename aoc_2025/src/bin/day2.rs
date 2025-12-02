729765f5a0952d959b695c804050579eba434241
Anders <anders.sandstrom89@gmail.com>
2025-12-02 21:37:52 +0100

    Optimize

--- aoc_2025/src/bin/day2.rs
+++ aoc_2025/src/bin/day2.rs
@@ -14,33 +14,26 @@ fn main() {
 
         for i in lower_int..=upper_int {
             let i_str = i.to_string();
-            let (start, end) = i_str.split_at(i_str.len()/2);
 
-            if start == end {
-                part1 += i;
-            }
-        }
-
-        for i in lower_int..=upper_int {
-            let i_str = i.to_string();
-
-            for size in 1..=i_str.len()/2 {
-                if (i_str.len() / size) * size != i_str.len(){
+            for size in (1..=i_str.len()/2).rev() {
+                if i_str.len()%size != 0{
                     continue;
                 }
-                let subs = i_str.as_bytes()
+                
+                let parts = i_str.as_bytes()
                     .chunks(size)
-                    .map(str::from_utf8)
-                    .collect::<Result<Vec<&str>, _>>()
-                    .unwrap();
+                    .collect::<Vec<&[u8]>>();
 
-                let first = subs[0];
-                if subs.iter().all(|&item| item == first) {
+                let first = parts[0];
+                if parts.iter().all(|&item| item == first) {
                     part2 += i;
+
+                    if i_str.len()%2 == 0 && i_str.len()/2 == size {
+                        part1 += i;
+                    }
                     break;
                 }
             }
-
         }
     }
 
