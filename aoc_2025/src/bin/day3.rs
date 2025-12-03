aa4a584710860549094412379237c3a23c358841
Anders <anders.sandstrom89@gmail.com>
2025-12-03 23:13:15 +0100

    Day3 refactored

--- aoc_2025/src/bin/day3.rs
+++ aoc_2025/src/bin/day3.rs
@@ -1,54 +1,36 @@
-
 fn main() {
-    let lines = include_str!("../../input_day3").lines();
+    let lines = include_str!("../../input_day3").lines().map(|line| {
+        line.chars()
+            .map(|c| c.to_digit(10).unwrap() as u64)
+            .collect::<Vec<u64>>()
+    });
 
     let mut part1 = 0;
     let mut part2 = 0;
 
-    for line in lines {
-        let parsed = line
-            .chars()
-            .map(|c| c.to_digit(10).unwrap() as u64)
-            .collect::<Vec<u64>>();
-
-        let mut index = 0;
-        for i in 0..parsed.len() - 1 {
-            if parsed[i] > parsed[index] {
-                index = i;
-            }
-        }
-
-        let mut index2 = index + 1;
-        for i in index + 1..parsed.len() {
-            if parsed[i] > parsed[index2] {
-                index2 = i;
-            }
-        }
-
-        let jolt = parsed[index] * 10 + parsed[index2];
-        part1 += jolt;
-
-        let mut indexes = [0usize; 12];
-
-        for i in 0..12 {
-            let start_index = if i == 0 { 0 } else { indexes[i - 1] + 1 };
-            let mut index = start_index;
-
-            for j in start_index..parsed.len() - (11 - i) {
-                if parsed[j] > parsed[index] {
-                    index = j;
-                }
-            }
-            indexes[i] = index;
-        }
-
-        let mut jolt = 0;
-        for i in 0..12 {
-            jolt += parsed[indexes[i]] * 10u64.pow((11 - i) as u32)
-        }
-        part2 += jolt;
+    for bank in lines {
+        part1 += calc_jolt(&bank, 2);
+        part2 += calc_jolt(&bank, 12);
     }
 
     println!("{}", part1);
     println!("{}", part2);
+}
+
+fn calc_jolt(bank: &Vec<u64>, number_of_batteries: usize) -> u64 {
+    let mut indexes = vec![0usize; number_of_batteries];
+
+    for i in 0..number_of_batteries {
+        let mut index = if i == 0 { 0 } else { indexes[i - 1] + 1 };
+        for j in index..bank.len() - (number_of_batteries - 1 - i) {
+            if bank[j] > bank[index] {
+                index = j;
+            }
+        }
+        indexes[i] = index;
+    }
+
+    (0..number_of_batteries)
+        .map(|i| bank[indexes[i]] * 10u64.pow((number_of_batteries - 1 - i) as u32))
+        .sum::<u64>()
 }
