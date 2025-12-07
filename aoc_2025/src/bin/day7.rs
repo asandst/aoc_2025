d05e302e80189caeee9b4a75b0d95f43cb0c4d26
Anders <anders.sandstrom89@gmail.com>
2025-12-07 10:51:23 +0100

    Day7

--- aoc_2025/src/bin/day7.rs
+++ aoc_2025/src/bin/day7.rs
@@ -1,49 +1,31 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashMap;
 
 fn main() {
     let input = include_str!("../../input_day7");
-
-    let mut beam_indexes = HashSet::new();
+    let mut beam_history = HashMap::new();
     let mut part1 = 0;
 
-    let mut beam_indexes_with_hist = HashMap::new();
-
-    for (i, line) in input.lines().enumerate() {
-        if i % 2 == 1 {
-            continue;
-        }
-
-        if beam_indexes.is_empty() {
-            let start_index = line.find("S").unwrap();
-            beam_indexes.insert(start_index);
-            beam_indexes_with_hist.insert(start_index, 1);
+    for line in input.lines() {
+        if beam_history.is_empty() {
+            beam_history.insert(line.find("S").unwrap(), 1);
         } else {
             for (x, c) in line.chars().enumerate() {
-                if c == '^' && beam_indexes.contains(&x) {
-                    beam_indexes.remove(&x);
-                    beam_indexes.insert(x - 1);
-                    beam_indexes.insert(x + 1);
-
-                    let x_value = beam_indexes_with_hist[&x];
-
-                    beam_indexes_with_hist
-                        .entry(x + 1)
-                        .and_modify(|e| *e += x_value)
-                        .or_insert(x_value);
-
-                    beam_indexes_with_hist
-                        .entry(x - 1)
-                        .and_modify(|e| *e += x_value)
-                        .or_insert(x_value);
-
-                    beam_indexes_with_hist.remove(&x);
+                if c == '^' && beam_history.contains_key(&x) {
+                    [x + 1, x - 1].iter().for_each(|key| {
+                        let value = beam_history[&x];
+                        beam_history
+                            .entry(*key)
+                            .and_modify(|e| *e += value)
+                            .or_insert(value);
+                    });
+                    beam_history.remove(&x);
                     part1 += 1;
                 }
             }
         }
     }
 
-    let part2 = beam_indexes_with_hist.values().sum::<i64>();
+    let part2 = beam_history.values().sum::<i64>();
 
     println!("{}", part1);
     println!("{}", part2);
