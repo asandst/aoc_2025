5f540d5295e3c4c76c993d05fd90aaa20c791016
Anders <anders.sandstrom89@gmail.com>
2025-12-08 22:12:15 +0100

    Day8

--- aoc_2025/src/bin/day8.rs
+++ aoc_2025/src/bin/day8.rs
@@ -13,7 +13,11 @@ fn main() {
         })
         .map(|v| (v[0], v[1], v[2]))
         .collect::<Vec<_>>();
+    println!("Part1 {}", part1(input.clone()));
+    println!("Part2 {}", part2(input));
+}
 
+fn part1(input: Vec<(i64, i64, i64)>) -> i64 {
     let mut connected = HashSet::<(usize, usize)>::new();
     let mut circuit = HashMap::<usize, usize>::new();
     let mut circuit_number: usize = 0;
@@ -31,15 +35,9 @@ fn main() {
                 }
             }
         }
-
-        //println!(
-        //    "Connect {:?} {:?}",
-        //    input[shortest_pair_index.0], input[shortest_pair_index.1]
-        //);
         connected.insert(shortest_pair_index);
         let last_circuit_number = circuit_number;
 
-        let mut c = 0;
         if circuit.contains_key(&shortest_pair_index.0)
             && circuit.contains_key(&shortest_pair_index.1)
         {
@@ -51,6 +49,7 @@ fn main() {
                 }
             }
         }
+        let c;
         if circuit.contains_key(&shortest_pair_index.0) {
             c = *circuit.get(&shortest_pair_index.0).unwrap();
         } else if circuit.contains_key(&shortest_pair_index.1) {
@@ -63,14 +62,69 @@ fn main() {
         circuit.insert(shortest_pair_index.0, c);
         circuit.insert(shortest_pair_index.1, c);
     }
-    println!("{:?}", circuit);
+
     let grouped = circuit.clone().into_iter().counts_by(|e| e.1);
     let res = grouped.values().k_largest(3);
-    let debug = circuit.values().sorted();
-    println!("{:?}", res);
-    println!("{:?}", res.product::<usize>());
-    println!("{:?}", debug);
+    res.product::<usize>() as i64
 }
+
+fn part2(input: Vec<(i64, i64, i64)>) -> i64 {
+    let mut connected = HashSet::<(usize, usize)>::new();
+    let mut circuit = HashMap::<usize, usize>::new();
+    let mut circuit_number: usize = 0;
+
+    loop {
+        let mut shortest_dist = i64::MAX;
+        let mut shortest_pair_index = (0, 0);
+        for i in 0..input.len() {
+            for j in i + 1..input.len() {
+                let pos1 = input[i];
+                let pos2 = input[j];
+                if dist(&pos1, &pos2) < shortest_dist && !connected.contains(&(i, j)) {
+                    shortest_dist = dist(&pos1, &pos2);
+                    shortest_pair_index = (i, j);
+                }
+            }
+        }
+
+        connected.insert(shortest_pair_index);
+        let last_circuit_number = circuit_number;
+
+        if circuit.contains_key(&shortest_pair_index.0)
+            && circuit.contains_key(&shortest_pair_index.1)
+        {
+            let c1 = circuit[&shortest_pair_index.0];
+            let c2 = circuit[&shortest_pair_index.1];
+            for value in circuit.values_mut() {
+                if *value == c2 {
+                    *value = c1;
+                }
+            }
+        }
+
+        let c;
+        if circuit.contains_key(&shortest_pair_index.0) {
+            c = *circuit.get(&shortest_pair_index.0).unwrap();
+        } else if circuit.contains_key(&shortest_pair_index.1) {
+            c = *circuit.get(&shortest_pair_index.1).unwrap();
+        } else {
+            c = last_circuit_number;
+            circuit_number += 1;
+        }
+
+        circuit.insert(shortest_pair_index.0, c);
+        circuit.insert(shortest_pair_index.1, c);
+
+        let grouped = circuit.clone().into_iter().counts_by(|e| e.1);
+        let res = grouped.values().k_largest(1).collect::<Vec<_>>();
+        if *res[0] == 1000 {
+            let pos1 = input[shortest_pair_index.0];
+            let pos2 = input[shortest_pair_index.1];
+            return pos1.0 * pos2.0;
+        }
+    }
+}
+
 fn dist((x1, y1, z1): &(i64, i64, i64), (x2, y2, z2): &(i64, i64, i64)) -> i64 {
     let p = (x1 - x2).pow(2) + (y1 - y2).pow(2) + (z1 - z2).pow(2);
     (p as f64).sqrt() as i64
